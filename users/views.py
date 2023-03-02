fcfd9cdc736630419ec5f6cb9d3b9be2404efc5e
sol6nik <80168295+sol6nik@users.noreply.github.com>
2023-03-02 12:22:31 +0300

    ended

---
+++ users/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.contrib import auth, messages
+from django.urls import reverse
+from products.models import Basket
+from django.contrib.auth.decorators import login_required
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title': 'Login',
+        'form': form,
+    }
+
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your registration has been successfully')
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+
+    context = {
+        'title': 'Registration',
+        'form': form
+    }
+    return render(request, 'users/register.html', context)
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+
+    baskets = Basket.objects.filter(user=request.user)
+
+    context = {
+        'title': 'Store - Profile',
+        'form': form,
+        'baskets': baskets,
+    }
+    return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
